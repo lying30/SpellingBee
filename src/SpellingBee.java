94ebd458fff0c4152bca858e3e06f5af70103005
Lucas_Ying <lucas.ying@menloschool.org>
2023-03-20 23:16:34 -0700

    as far as i got :( couldn't figure out how to fix my check words method

--- src/SpellingBee.java
+++ src/SpellingBee.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -38,6 +39,7 @@ public class SpellingBee {
     public SpellingBee(String letters) {
         this.letters = letters;
         words = new ArrayList<String>();
+
     }
 
     // TODO: generate all possible substrings and permutations of the letters.
@@ -45,14 +47,69 @@ public class SpellingBee {
     //  that will find the substrings recursively.
     public void generate() {
         // YOUR CODE HERE — Call your recursive method!
+        makeWords("", letters);
     }
 
+    public void makeWords(String word, String letters){
+        if (letters.length() == 0) {
+            words.add(word);
+        }
+
+        for (int i = 0; i < letters.length(); i++) {
+            String newPrefix = word + letters.charAt(i);
+            String newRemainingLetters = letters.substring(0, i) + letters.substring(i + 1);
+            makeWords(newPrefix, newRemainingLetters);
+        }
+        words.add(word);
+    }
     // TODO: Apply mergesort to sort all words. Do this by calling ANOTHER method
     //  that will find the substrings recursively.
     public void sort() {
         // YOUR CODE HERE
+        words = mergesort(words);
     }
 
+    public ArrayList<String> mergesort(ArrayList<String> arr) {
+        if(arr.size() == 1){
+            return arr;
+        }
+        int half = arr.size()/2;
+        ArrayList<String> firstHalf = new ArrayList<>();
+        ArrayList<String> secondHalf = new ArrayList<>();
+        for(int i = 0; i<half; i++){
+            firstHalf.add(arr.get(i));
+        }
+        for(int i = half; i<arr.size(); i++){
+            secondHalf.add(arr.get(i));
+        }
+        ArrayList<String> arr1 = mergesort(firstHalf);
+        ArrayList<String> arr2 = mergesort(secondHalf);
+
+        return merge(arr1, arr2);
+
+    }
+    public ArrayList<String> merge(ArrayList<String> arr1, ArrayList<String> arr2){
+        ArrayList<String> newArr = new ArrayList<String>();
+        while(arr1.size() != 0 && arr2.size() != 0){
+            if(arr1.get(0).compareTo(arr2.get(0)) < 0){
+                String str = arr1.remove(0);
+                newArr.add(str);
+            }
+            else {
+                String str = arr2.remove(0);
+                newArr.add(str);
+            }
+        }
+        if(arr1.isEmpty() && !(arr2.isEmpty())) {
+            newArr.addAll(newArr.size(), arr2);
+        }
+        else if(!(arr1.isEmpty()) && arr2.isEmpty()) {
+            newArr.addAll(newArr.size(), arr1);
+        }
+        else{}
+        return newArr;
+
+    }
     // Removes duplicates from the sorted list.
     public void removeDuplicates() {
         int i = 0;
@@ -68,7 +125,33 @@ public class SpellingBee {
     // TODO: For each word in words, use binary search to see if it is in the dictionary.
     //  If it is not in the dictionary, remove it from words.
     public void checkWords() {
-        // YOUR CODE HERE
+        boolean inDictionary;
+        for(int i = 0; i<words.size(); i++){
+            inDictionary = binarySearch(DICTIONARY, words.get(i), 0, words.size() - 1);
+            if(!inDictionary) {
+                words.remove(i);
+                i--;
+            }
+
+        }
+
+    }
+    //This method doesn't work because it only accesses the dictionary for words that start with an a.
+    //The other methods should work though.
+    public boolean binarySearch(String[] dic, String target, int low, int high) {
+        if (high < low) {
+            return false;
+        }
+        int mid = low + (high-low)/2;
+        if(dic[mid].equals(target)) {
+            return true;
+        }
+        else if (dic[mid].compareTo(target) > 0) {
+            return binarySearch(dic, target, low, mid-1);
+        }
+        else {
+            return binarySearch(dic, target, mid+1, high);
+        }
     }
 
     // Prints all valid words to wordList.txt
